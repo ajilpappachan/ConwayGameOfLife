640b9c8578818be94b3bc8bcbd9be618acc7f564
Ajil Pappachan <ajilpappachan@gmail.com>
2021-02-15 17:35:39 +0530

    created Game Board

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    private bool isAlive = false;
+    private int numNeighbours = 0;
+    private GameController controller;
+
+    public void cellInit()
+    {
+        controller = FindObjectOfType<GameController>();
+        GetComponent<SpriteRenderer>().color = controller.deadCellColor;
+    }
+
+    public void setNeighbours(int num)
+    {
+        //Rules:
+        //1.Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        //2.Any live cell with two or three live neighbours lives on to the next generation.
+        //3.Any live cell with more than three live neighbours dies, as if by overpopulation.
+        //4.Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+        numNeighbours = num;
+        if(isAlive)
+        {
+            if(num < 2 || num > 3)
+            {
+                setAlive(false);
+            }
+        }
+        else
+        {
+            if(num == 3)
+            {
+                setAlive(true);
+            }
+        }
+    }
+
+    public void setAlive(bool value)
+    {
+        isAlive = value;
+        if (value)
+            GetComponent<SpriteRenderer>().color = controller.aliveCellColor;
+        else
+            GetComponent<SpriteRenderer>().color = controller.deadCellColor;
+    }
+
+    public bool IsAlive()
+    {
+        return isAlive;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Grid Setup")]
+    [SerializeField] private int width = 6;
+    [SerializeField] private int height = 6;
+    [SerializeField] private GameObject cellObject;
+    [SerializeField] private GameObject gridParent;
+    [SerializeField] private float stepDuration = 0.1f;
+    private Cell[,] grid;
+    private float timer;
+
+    [Header("Cell Properties")]
+    public Color deadCellColor;
+    public Color aliveCellColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Cell[width, height];
+        gridInit();
+        randomPattern();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > stepDuration)
+        {
+            timer = 0;
+            updateCells();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
+    }
+
+    private void gridInit()
+    {
+        //Create Cells
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                GameObject cell = Instantiate(cellObject, new Vector2(x, y), Quaternion.identity);
+                cell.GetComponent<Cell>().cellInit();
+                cell.transform.SetParent(gridParent.transform);
+                grid[x, y] = cell.GetComponent<Cell>();
+            }
+        }
+
+        //Set Camera to Center of the Grid and fit the grid inside view
+        Camera.main.transform.position = new Vector3(width / 2, height / 2, -10f);
+        Camera.main.orthographicSize = width > height ? width / 2 : height / 2;
+    }
+
+    private void updateCells()
+    {
+        //Rules:
+        //1.Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        //2.Any live cell with two or three live neighbours lives on to the next generation.
+        //3.Any live cell with more than three live neighbours dies, as if by overpopulation.
+        //4.Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int liveNeighbours = checkNeighbours(x, y);
+                grid[x, y].setNeighbours(liveNeighbours);
+            }
+        }
+    }
+
+    //Get number of live neighbours
+    private int checkNeighbours(int x, int y)
+    {
+        int liveNeighbours = 0;
+        if (x > 0)
+            liveNeighbours += grid[x - 1, y].IsAlive() ? 1 : 0;
+        if (x < width - 1)
+            liveNeighbours += grid[x + 1, y].IsAlive() ? 1 : 0;
+        if (y > 0)
+            liveNeighbours += grid[x, y - 1].IsAlive() ? 1 : 0;
+        if (y < height - 1)
+            liveNeighbours += grid[x, y + 1].IsAlive() ? 1 : 0;
+        if (x > 0 && y > 0)
+            liveNeighbours += grid[x - 1, y - 1].IsAlive() ? 1 : 0;
+        if (x < width - 1 && y < height - 1)
+            liveNeighbours += grid[x + 1, y + 1].IsAlive() ? 1 : 0;
+        if (x > 0 && y < height - 1)
+            liveNeighbours += grid[x - 1, y + 1].IsAlive() ? 1 : 0;
+        if (x < width - 1 && y > 0)
+            liveNeighbours += grid[x + 1, y - 1].IsAlive() ? 1 : 0;
+        return liveNeighbours;
+    }
+
+    private void randomPattern()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int randomValue = Mathf.RoundToInt(Random.Range(0.0f, 1.0f));
+                if (randomValue == 1)
+                    grid[x, y].setAlive(true);
+                else
+                    grid[x, y].setAlive(false);
+            }
+        }
+    }
+
+}
