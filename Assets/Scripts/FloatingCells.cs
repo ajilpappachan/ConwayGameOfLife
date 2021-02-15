8d4f5d1fe55de89f7756557a2c4b89f15729f854
Ajil Pappachan <ajilpappachan@gmail.com>
2021-02-15 18:07:44 +0530

    Basic UI

---
+++ Assets/Scripts/FloatingCells.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingCells : MonoBehaviour
+{
+    ParticleSystem ps;
+    public int spawnRate = 3;
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ParticleSystem.MainModule main = ps.main;
+        main.startSpeed = speed;
+        ParticleSystem.EmissionModule emission = ps.emission;
+        emission.rateOverTime = spawnRate;
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject pauseButton;
+    public GameObject resumeButton;
+    public GameObject quitButton;
+
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+        pauseButton.SetActive(false);
+        resumeButton.SetActive(true);
+        quitButton.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        //If main menu, quit the game. Otherwise go to main menu
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+            Application.Quit();
+        else
+            SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        pauseButton.SetActive(true);
+        resumeButton.SetActive(false);
+        quitButton.SetActive(false);
+    }
+}
